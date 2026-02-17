0b9ad4191b776e33529a408f45b577a5ad6dfe79
rgbailon <rgbailon@example.com>
2026-02-17 20:59:40 +0800

    Add fast search - searches JSON data instantly

--- scraper_app/views.py
+++ scraper_app/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
-from .models import Playlist, Video
 from .services.scraper import (
     load_from_json,
     get_playlist_by_id,
@@ -10,7 +8,7 @@ from .services.scraper import (
 
 
 def home(request):
-    """Home page - show recent playlists from JSON"""
+    """Home page - show all playlists from JSON"""
     recent_data = load_from_json()
     playlists = recent_data.get("playlists", []) if recent_data else []
 
@@ -21,35 +19,36 @@ def home(request):
     return render(request, "scraper_app/home.html", context)
 
 
+def search_results(request):
+    """Search playlists from JSON data"""
+    query = request.GET.get("q", "").lower().strip()
+
+    recent_data = load_from_json()
+    all_playlists = recent_data.get("playlists", []) if recent_data else []
+
+    if query:
+        filtered_playlists = [
+            p for p in all_playlists if query in p.get("title", "").lower()
+        ]
+    else:
+        filtered_playlists = all_playlists
+
+    context = {
+        "playlists": filtered_playlists,
+        "query": query,
+        "recent_data": recent_data,
+    }
+    return render(request, "scraper_app/home.html", context)
+
+
 def playlist_detail(request, playlist_id):
-    """Display playlist details with all videos from JSON"""
+    """Display playlist details from JSON"""
     playlist = get_playlist_by_id(playlist_id)
 
     if not playlist:
-        try:
-            db_playlist = Playlist.objects.get(playlist_id=playlist_id)
-            videos = db_playlist.videos.all()
-            playlist = {
-                "playlist_id": db_playlist.playlist_id,
-                "title": db_playlist.title,
-                "url": db_playlist.url,
-                "video_count": db_playlist.video_count,
-                "thumbnail": db_playlist.get_thumbnail(),
-                "videos": [
-                    {
-                        "video_id": v.video_id,
-                        "title": v.title,
-                        "url": v.url,
-                        "thumbnail": v.get_thumbnail(),
-                        "position": v.position,
-                    }
-                    for v in videos
-                ],
-            }
-        except Playlist.DoesNotExist:
-            from django.http import Http404
+        from django.http import Http404
 
-            raise Http404("Playlist not found")
+        raise Http404("Playlist not found")
 
     context = {
         "playlist": playlist,
@@ -62,22 +61,9 @@ def video_player(request, video_id):
     video, playlist = get_video_by_id(video_id)
 
     if not video:
-        db_video = Video.objects.filter(video_id=video_id).first()
-        if db_video:
-            video = {
-                "video_id": db_video.video_id,
-                "title": db_video.title,
-                "url": db_video.url,
-                "thumbnail": db_video.get_thumbnail(),
-            }
-            playlist = {
-                "playlist_id": db_video.playlist.playlist_id,
-                "title": db_video.playlist.title,
-            }
-        else:
-            from django.http import Http404
+        from django.http import Http404
 
-            raise Http404("Video not found")
+        raise Http404("Video not found")
 
     related_videos = []
     if playlist and "videos" in playlist:
@@ -91,26 +77,3 @@ def video_player(request, video_id):
         "related_videos": related_videos,
     }
     return render(request, "scraper_app/video_player.html", context)
-
-
-def search_results(request):
-    """Disabled - scraping not available"""
-    return redirect("home")
-
-
-@require_http_methods(["POST"])
-def start_search(request):
-    """Disabled - scraping not available"""
-    return JsonResponse(
-        {"success": False, "error": "Scraping not available on this deployment"}
-    )
-
-
-def get_search_results(request):
-    """AJAX endpoint to get search results"""
-    data = load_from_json()
-
-    if not data:
-        return JsonResponse({"success": False, "error": "No data available"})
-
-    return JsonResponse({"success": True, "data": data})
